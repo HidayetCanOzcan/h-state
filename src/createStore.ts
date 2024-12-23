cd151ff365547a739f94cc69f833639485d8584b
Hidayet Can Özcan <67768604+HidayetCanOzcan@users.noreply.github.com>
2024-12-23 08:08:36 +0300

    fix(conditional): stabilize hook calls

--- src/createStore.ts
+++ src/createStore.ts
@@ -16,40 +16,47 @@ export function createStore<
   const reactiveStore = new ReactiveStore(initial);
   const store = reactiveStore.getProxy() as StoreType<T, M>;
   let version = 0;
+  let stableSubscribers = new Set<() => void>();
 
-  (store as StoreType<T, M> & { merge: (partial: Partial<T>) => void }).merge = (
-    partial: Partial<T>
-  ) => {
-    reactiveStore.mergeDeep(store, partial);
-    reactiveStore.notifySubscribers();
-  };
-
+  // Store metodlarını oluştur
   Object.keys(methodCreators).forEach((methodName) => {
     const creator = methodCreators[methodName];
     const method = creator(store);
     Object.assign(store, { [methodName]: method });
   });
 
-  const subscribe = (callback: () => void) => {
-    return reactiveStore.subscribe(callback);
+  // Stable subscription
+  const stableSubscribe = (callback: () => void) => {
+    stableSubscribers.add(callback);
+    return () => {
+      stableSubscribers.delete(callback);
+    };
   };
 
+  // Store snapshot
   const getSnapshot = () => {
     return version;
   };
 
+  // Server snapshot for SSR
   const getServerSnapshot = () => {
     return version;
   };
 
+  // Subscribe to store updates
   reactiveStore.subscribe(() => {
     version++;
+    stableSubscribers.forEach(callback => callback());
   });
 
-  function useStore() {
-    useSyncExternalStore(subscribe, getSnapshot, getServerSnapshot);
+  const stableUseStore = () => {
+    useSyncExternalStore(stableSubscribe, getSnapshot, getServerSnapshot);
     return store;
-  }
+  };
 
-  return { Store: store, useStore, subscribe };
+  return { 
+    Store: store, 
+    useStore: stableUseStore, 
+    subscribe: stableSubscribe 
+  };
 }
